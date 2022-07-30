4572efed23fe07620636862451343d5818f05999
Husnain Ashraf <hashraf@globalrescue.com>
2022-07-31 03:47:09 +0500

    Search Control added with fiter popup

--- pages/dashboard/school.js
+++ pages/dashboard/school.js
@@ -19,7 +19,7 @@ function SchoolDashboard() {
             <div className="wrapper">
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="offset-9 col-3">
+                        <div className="offset-lg-9 col-12 col-lg-3 mb-3 mb-lg-0">
                             <div className="input-group">
                                 <button className="btn btn-search" type="button"><img src="../icons/quick-search.png" /></button>
                                 <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="" />
