4fef712e7659e46f7cd8b0ed3571adc7281e91da
Husnain Ashraf <hashraf@globalrescue.com>
2022-08-01 02:41:20 +0500

    Search bar fixed for dashboard

--- components/dashboardReport/dashboardReport.js
+++ components/dashboardReport/dashboardReport.js
@@ -6,15 +6,6 @@ class DashboardReport extends Component {
         return (
             <>
                 <div className='wrapper-section'>
-                    <div className="row">
-                        <div className="offset-lg-9 col-12 col-lg-3 mb-3 mb-lg-0">
-                            <div className="input-group">
-                                <button className="btn btn-search" type="button"><img src="../icons/quick-search.png" /></button>
-                                <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="" />
-                                <button onClick={() => setModalShow(true)} className="btn btn-filter" type="button"><img src="../icons/filter.png" /></button>
-                            </div>
-                        </div>
-                    </div>
                     <EnhancedTable></EnhancedTable>
                 </div>
             </>
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -15,39 +15,48 @@ function AdminPage() {
                 <AdminSideNav></AdminSideNav>
             </aside>
             <div className="wrapper">
-            <Tabs
-                defaultActiveKey="home"
-                className="mb-3"
-                >
-                <Tab eventKey="home" title={
-                <>
-                    <span className="badge">15</span> Stem Club
-                </>}
-                >
-                <DashboardReport></DashboardReport>
-                </Tab>
-                <Tab eventKey="profile" title={
-                <>
-                    <span className="badge">17</span> Stem Safeer
-                </>}
-                >
-                <DashboardReport></DashboardReport>
-                </Tab>
-                <Tab eventKey="contact" title={
-                <>
-                    <span className="badge">24</span> Teacher Hub
-                </>}
-                >
-                <DashboardReport></DashboardReport>
-                </Tab>
-                <Tab eventKey="Others" title={
-                <>
-                    <span className="badge">08</span> Others
-                </>}
-                >
-                <DashboardReport></DashboardReport>
-                </Tab>
-            </Tabs>
+                <div className="row">
+                    <div className="offset-lg-9 col-12 col-lg-3 mb-3 mb-lg-0">
+                        <div className="input-group">
+                            <button className="btn btn-search" type="button"><img src="../icons/quick-search.png" /></button>
+                            <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="" />
+                            <button onClick={() => setModalShow(true)} className="btn btn-filter" type="button"><img src="../icons/filter.png" /></button>
+                        </div>
+                    </div>
+                </div>
+                <Tabs
+                    defaultActiveKey="home"
+                    className="mb-3"
+                    >
+                    <Tab eventKey="home" title={
+                    <>
+                        <span className="badge">15</span> Stem Club
+                    </>}
+                    >
+                    <DashboardReport></DashboardReport>
+                    </Tab>
+                    <Tab eventKey="profile" title={
+                    <>
+                        <span className="badge">17</span> Stem Safeer
+                    </>}
+                    >
+                    <DashboardReport></DashboardReport>
+                    </Tab>
+                    <Tab eventKey="contact" title={
+                    <>
+                        <span className="badge">24</span> Teacher Hub
+                    </>}
+                    >
+                    <DashboardReport></DashboardReport>
+                    </Tab>
+                    <Tab eventKey="Others" title={
+                    <>
+                        <span className="badge">08</span> Others
+                    </>}
+                    >
+                    <DashboardReport></DashboardReport>
+                    </Tab>
+                </Tabs>
             </div>
         </Fragment>
     )
