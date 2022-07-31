48e83fa5e34539e6bf18e777f9f15485e931e418
Husnain Ashraf <hashraf@globalrescue.com>
2022-08-01 02:30:06 +0500

    UI Fixes

--- components/settings/schoolsNotVerified.js
+++ components/settings/schoolsNotVerified.js
@@ -7,7 +7,7 @@ class SchoolsNotVerified extends Component {
             <div className='wrapper-section p-5'>
             <div className="row">
                     <div className="col-lg-3 mb-3">
-                        <div className="input-group border rounded">
+                        <div className="input-group border rounded pe-3">
                             <button className="btn btn-search" type="button"><img src="../icons/quick-search.png" /></button>
                             <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="" />
                         </div>
